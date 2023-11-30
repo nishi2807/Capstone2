800e162bdd1a3f5d72b454dd554c9ddc3185a0ed
arnav2305 <arnav.pandey20@st.niituniversity.in>
2023-11-30 12:06:56 +0530

    Test1

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';              //Test 1
 import App from './App';
 
 test('renders learn react link', () => {
